a4bc4124989fe28c2b403497e734f95bb01bf40d
Jodi Williams <jodi.williams@aggiemail.usu.edu>
2019-07-28 00:01:28 -0600

    changes to accommodate both edit and create

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Edit post {{$climb['id']}}</h1>
+<h1>Edit {{$climb['name']}}</h1>
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+<form action="/admin/climb/{{ $climb['id'] }}/edit" method="post">
     @include('partials.climb-form')
+    <div class="form-group">
+        <input type="submit" value="Update Route">
+    </div>
+    <!--end .form-group -->
+</form>
 @endsection
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,13 +2,19 @@
 
 @section('content')
 <h1>Admin Homepage</h1>
-@foreach ($climbs['climbs'] as $climb)
-<li>
-    {{ $climb['name'] }}
-    <a href="{{ route('admin.climb', ['id' => $climb['id']]) }}">View</a>
-    <a href="{{ route('admin.edit', ['id' => $climb['id']]) }}">Edit</a>
-    <a href="{{ route('admin.delete', ['id' => $climb['id']]) }}">Delete</a>
-</li>
-@endforeach
+<div class="mt-4">
+    <a class="btn btn-success" href="{{ route('admin.create') }}">Create Climb</a>
+    <br>
+</div>
+<div class="mt-3">
+    @foreach ($climbs['climbs'] as $climb)
+    <li>
+        {{ $climb['name'] }}
+        <a href="{{ route('admin.climb', ['id' => $climb['id']]) }}">View</a>
+        <a href="{{ route('admin.edit', ['id' => $climb['id']]) }}">Edit</a>
+        <a href="{{ route('admin.delete', ['id' => $climb['id']]) }}">Delete</a>
+    </li>
+    @endforeach
+</div>
 
 @endsection
